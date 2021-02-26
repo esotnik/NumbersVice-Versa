0a7c3d4bae42a12dabf28ca1b8e2c3dcdd7a0725
Eve <e_sotnik@bk.ru>
2021-02-26 20:38:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NumbersViceVersa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 15;
+
+            Console.WriteLine($"{a}-{b}");
+
+            (a, b) = (b, a); // обмен
+
+            Console.WriteLine($"{a}-{b}");
+        }
+    }
+}
